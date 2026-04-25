ef9ca012bd54ac1745f59811bdc39cf4f91f7001
Ajit <ajiteshgusain@gmail.com>
2026-04-25 15:51:42 +0530

    refactor: clean up unused dependencies and improve click handler formatting, and undstanding  what errors can come when  we render our component

--- practice_react_day_19/src/App.jsx
+++ practice_react_day_19/src/App.jsx
@@ -2,16 +2,20 @@ import { useState } from "react";
 
 function App(){
   const[para,setPara]=useState("welcome to javascript")
-}
+  
+  
+  
+  
+  function clickHandler(){
+    
+    if(para ==="welcome to javascript")
+      {setPara("welcome to feel the magic of react js")
 
-function clickHandler(){
-  if(para ==="welcome to javascript"){
-    setPara("welcome to feel the magic of react js")
-
-  }else{
-    setPara("welcome to javascript")
-
-  }
+      }else{
+        setPara("welcome to javascript")
+      
+      }
+    }
 
   return(
     <div>
@@ -20,8 +24,36 @@ function clickHandler(){
 
 
     </div>
-  )
+  );
 
 }
 
+
 export default App 
+
+
+// import { useState } from "react";
+
+// function App() {
+//   // 1. State must be inside the component
+//   const [para, setPara] = useState("welcome to javascript");
+
+//   // 2. Functions that use that state must also be inside
+//   function clickHandler() {
+//     if (para === "welcome to javascript") {
+//       setPara("welcome to feel the magic of react js");
+//     } else {
+//       setPara("welcome to javascript");
+//     }
+//   }
+
+//   // 3. The return UI must be inside
+//   return (
+//     <div>
+//       <p>{para}</p>
+//       <button onClick={clickHandler}>click me please</button>
+//     </div>
+//   );
+// }
+
+// export default App; // Don't forget to export it!
