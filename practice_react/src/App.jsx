2214f8255ae5bc0eeebfef6b8330e44735afa0b1
Ajit <ajiteshgusain@gmail.com>
2026-05-02 14:49:50 +0530

    feat: initialize React project with Vite, add basic structure and components
    - Created package.json with dependencies and scripts for development
    - Added favicon.svg and icons.svg for application branding
    - Implemented App component with a greeting and PracticeReact component
    - Created PracticeReact component to display a welcome message
    - Added global styles in index.css for consistent theming
    - Set up main entry point in main.jsx for rendering the application
    - Configured Vite with react plugin in vite.config.js

---
+++ practice_react/src/App.jsx
@@ -0,0 +1,18 @@
+import PracticeReact from "./components/PracticeReact";
+
+function  App(){
+  return(
+    <div>
+      <h1>
+        hello you are cool react , you !! react nicely 
+      </h1>
+
+      <p><PracticeReact/></p>
+    </div>
+  )
+
+
+}
+
+
+export default App;
---
+++ practice_react/src/components/PracticeReact.jsx
@@ -0,0 +1,9 @@
+
+
+
+function PracticeReact(){
+    return(
+        <h2>welcome to React</h2>
+    )
+}
+export default PracticeReact;
