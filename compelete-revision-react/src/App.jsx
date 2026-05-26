a2515817f36175e51434193b3c9cf1d13eb96ca5
Ajit <ajiteshgusain@gmail.com>
2026-05-26 13:46:22 +0530

    feat: refactor App component and add Greet component for user greeting functionality

--- compelete-revision-react/src/App.jsx
+++ compelete-revision-react/src/App.jsx
@@ -1,46 +1,96 @@
-import React,{useState} from "react";
+// import React,{useState} from "react";
 
 
 
-function App(){
+// function App(){
 
-  const[coubt,setcount]=useState(0);
+//   const[coubt,setcount]=useState(0);
 
-  const btn={padding:"6px 12px",margin:"5px"
-    ,fontsize:"30px"
-  }
+//   const btn={padding:"6px 12px",margin:"5px"
+//     ,fontsize:"30px"
+//   }
 
-  const increase=()=>{
-    setcount(count+1)}
-
-
-  const decrease=()=>{
-    setcount(count-1)}
+//   const increase=()=>{
+//     setcount(count+1)}
 
 
-  const reset=()=>{
-    setcount(0)
-  }
+//   const decrease=()=>{
+//     setcount(count-1)}
 
 
+//   const reset=()=>{
+//     setcount(0)
+//   }
+
+
+
+//   return(
+
+
+//     <div style={{textAlign:"center"}}>
+
+//       <h2>count:{count}</h2>
+
+//       <button style={btn} onClick={increase}>+</button>
+
+//       <button style={btn} onClick={decrease}>-</button>
+
+//       <button style={btn} onClick={reset}>reset</button>
+
+//     </div>
+//   );
+
+// }
+
+
+// export default App;
+
+
+
+// import React from "react";
+// import Greet from "./components/Greet";
+
+// function App(){
+
+//   return(
+//     <div>
+
+//       <Greet name="liv"/>
+//       <Greet name="shosusa"/>
+//     </div>
+//   );
+
+
+
+
+// }
+
+// export default App;
+
+
+
+import React from "react";
+
+function  App(){
+
+  const islogged=true;
+  // if (islogged){
+  //   return <h1>welcome user</h1>;
+
+  // }else{
+
+
+  //   return <h2>please login</h2>
+
+  // };
 
   return(
-
-
-    <div style={{textAlign:"center"}}>
-
-      <h2>count:{count}</h2>
-
-      <button style={btn} onClick={increase}>+</button>
-
-      <button style={btn} onClick={decrease}>-</button>
-
-      <button style={btn} onClick={reset}>reset</button>
-
+    <div>
+      {islogged ?<h1>welcome user</h1> : <h1>please login</h1>}
     </div>
   );
 
+
+
+
 }
-
-
-export default App;
---
+++ compelete-revision-react/src/components/Greet.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Greet(){
+
+    return (
+    
+    <h1>hello ,{props.name}</h1>
+    
+)
+}
+
+
+
+export default  Greet;
